778e0afeee4d489362f0829196d798d567db7709
Sweatha Bharathi <sweatha.bharathi@syncfusion.com>
2025-05-09 16:49:26 +0530

    How to get the edited records in WinForms DataGrid

---
+++ WinformsDataGrid/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Syncfusion.WinForms.DataGrid;
+
+namespace WinformsDataGrid
+{
+    public partial class Form1 : Form
+    {
+        OrderInfoCollection orderInfoCollection;
+        ObservableCollection<OrderInfo> editedRows;
+        ObservableCollection<object> editedRecords;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            sfDataGrid1.Columns.Add(new GridTextColumn() { MappingName = "OrderID", HeaderText = "Order ID" });
+            sfDataGrid1.Columns.Add(new GridTextColumn() { MappingName = "CustomerID", HeaderText = "Customer ID" });
+            sfDataGrid1.Columns.Add(new GridTextColumn() { MappingName = "CustomerName", Width = 150, HeaderText = "Customer Name" });
+            sfDataGrid1.Columns.Add(new GridTextColumn() { MappingName = "Country", HeaderText = "Country" });
+            sfDataGrid1.Columns.Add(new GridTextColumn() { MappingName = "ShipCity", HeaderText = "Ship City" });
+            orderInfoCollection = new OrderInfoCollection();
+            editedRows = new ObservableCollection<OrderInfo>();
+            editedRecords = new ObservableCollection<object>();
+            sfDataGrid1.DataSource = orderInfoCollection.Orders;
+
+            sfDataGrid1.CurrentCellValidated += SfDataGrid1_CurrentCellValidated;
+          
+        }
+
+        private void SfDataGrid1_CurrentCellValidated(object sender, Syncfusion.WinForms.DataGrid.Events.CurrentCellValidatedEventArgs e)
+        {
+            if (e.NewValue != null)
+            {
+                editedRecords.Add(e.NewValue);
+            }
+            if (e.RowData != null)
+            {
+                editedRows.Add(e.RowData as OrderInfo);
+            }
+        }
+    }
+}
